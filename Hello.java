5754677723b7661799f709a3d79fbaaeac705c59
Gustavo Valença Marcilio <workgustavo10@gmail.com>
2024-07-03 14:29:51 -0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello world!");
+        System.out.println("Você não tem inimigos! Ninguém tem...");
+        System.out.println("Não existe pessoas desempregas, mas sim, protetores de lares!");
+        System.out.println("Números: " + (1 + 2));
+        System.out.println("Decimais " + (0.2356 - 0.6532));
+        System.out.println("Booleans " + (true & false));
+
+    }
+}
